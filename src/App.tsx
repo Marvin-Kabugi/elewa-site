47db314c640846aec9d3fdcc607c0c46781d3607
Marvin-Kabugi <kabugijeff@gmail.com>
2023-11-02 16:00:41 +0300

    :sparkles: feat : added image-landing components

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
-
+import Landing from './components/Landing';
 function App() {
   const links: string[] = ['Home', 'About Us', 'Social Impact', 'Invest', 'Venture Labs', 'Brands', 'Careers', 'Contact']
   return (
     <div className="App">
       <header className="App-header">
         <NavBar links={links}/>
+        <Landing />
      </header>
     </div>
   );
